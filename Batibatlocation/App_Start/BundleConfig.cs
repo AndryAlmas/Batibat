a22af86535539c68e7633b07dd9cfb19bafaffe1
Andry Almas <andry.almas@gmail.com>
2024-12-10 12:08:53 +0100

    aggiunta di pulsanti e funzionalità alla section Services

--- Batibatlocation/App_Start/BundleConfig.cs
+++ Batibatlocation/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace Batibatlocation
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            "~/Scripts/jquery-{version}.slim.min.js"));
+            "~/Scripts/jquery-3.5.1.slim.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            "~/Scripts/bootstrap-{version}.min.js"));
+            "~/Scripts/bootstrap-4.5.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/popper").Include(
-            "~/Scripts/popper-{version}.min.js"));
+            "~/Scripts/popper-2.5.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/fontawesome").Include(
-            "~/Scripts/FontAwesome.js"));
+            "~/Scripts/FontAwesome-6.7.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/custom").Include(
             "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Styles/bootstrap-{version}.min.css",
+                      "~/Styles/bootstrap-4.5.2.min.css",
                       "~/Styles/site.css"));
         }
     }
