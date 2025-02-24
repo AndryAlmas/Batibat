6d8bf7f46b98eb7fb6510c62035b73a1b98f3802
Andry Almas <andry.almas@gmail.com>
2025-02-24 19:54:56 +0100

    gestione accesso pannello amminsitratore e pulsante sulla homepage

--- Batibatlocation/App_Start/RouteConfig.cs
+++ Batibatlocation/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Batibatlocation
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AdminRoute",
+                url: "Admin",
+                defaults: new { controller = "Admin", action = "Dashboard" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
